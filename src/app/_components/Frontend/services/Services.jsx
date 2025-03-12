086bcaf4fd879d326ffd1ed3aac38b1107b4c0f3
NikhilSharma-007 <niks21902@gmail.com>
2025-03-12 18:35:16 +0530

    Added case study, testimonials and contact us section in services

--- src/app/_components/Frontend/services/Services.jsx
+++ src/app/_components/Frontend/services/Services.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Hero from "./parts/Hero.jsx";
 import Coaching from "./parts/Coaching.jsx";
+import Case_Study from "./parts/Case_Study.jsx";
+import Contact_us from "../about/parts/Contact_us.jsx";
+import Testimonials from "../home/parts/Testimonials.jsx";
 
 const Services = () => {
   return (
     <div className="p-14">
       <Hero />
       <Coaching />
+      <Case_Study />
+      <Testimonials />
+      <Contact_us />
     </div>
   );
 };
