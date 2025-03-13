5cd227258bfd7b43575e1040b6d3ad5a279da2c7
NikhilSharma-007 <niks21902@gmail.com>
2025-03-13 16:47:16 +0530

    Add hero and program section in coaching skills

--- src/app/_components/Frontend/coaching_skills/Coaching_skills.jsx
+++ src/app/_components/Frontend/coaching_skills/Coaching_skills.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Hero from "./parts/Hero";
 import Contact_us from "../about/parts/Contact_us";
+import Program from "./parts/Program";
 
 const Coaching_supervision = () => {
   return (
     <div className="p-14">
       <Hero />
+      <Program />
       <Contact_us
-        heading="Want to Take Your Coaching to the Next Level?"
+        heading="Ready to Transform Your Leadership?"
         line1="Take the first step toward building a coaching culture in your organization. Join our Coaching Skills Program and become the leader who inspires, empowers, and drives meaningful change."
         line2="Get in Touch Now to book a consultation and explore how this program can elevate your leadership."
       />
