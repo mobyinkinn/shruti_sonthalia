53e0273fb5d211b93ae5f97a376c7b192d2fe6e7
jatin2040947 <jatin@mobyink.com>
2025-03-29 18:24:29 +0530

    .

--- src/app/_components/Frontend/change_management/Change-management.jsx
+++ src/app/_components/Frontend/change_management/Change-management.jsx
@@ -11,12 +11,18 @@ const Change = () => {
   return (
     <div className="p-14 bg-[#FCFCFF]">
       <Hero />
-      <Workshop/>
-      <Sliders/>
-      <WorkshopBenefits/>
-      <WhyStandApart/>
-      <WorkshopOutcome/>
-      <ExecutiveCoaching/>
+      <Workshop />
+      <Sliders />
+      <WorkshopBenefits />
+      <WhyStandApart />
+      <WorkshopOutcome />
+      <ExecutiveCoaching />
+      <Contact_us
+        heading="Take the First Step Toward Successful Change"
+        line1="Don’t let your change initiatives fall into the 70% that fail. Are you ready to turn change into  a competitive advantage?"
+        line2="Sign up for Our Change Management Programs Now"
+        button_Name="Get In Touch"
+      />
     </div>
   );
 }
--- src/app/_components/Frontend/pioneering/Pioneering.jsx
+++ src/app/_components/Frontend/pioneering/Pioneering.jsx
@@ -3,18 +3,23 @@ import Hero from './parts/Hero'
 import ScrollTimeline from './parts/Dashed';
 import Leftside from './parts/Leftside';
 import Rightside from './parts/Rightside';
-import ContactSection from '../home/parts/Contact';
+import Contact_us from '../about/parts/Contact_us';
 
 const Pioneering = () => {
   return (
     <div className="px-14 bg-[#FCFCFF]">
       <Hero />
-      <div className='flex flex-row justify-between'> 
+      <div className="flex flex-row justify-between">
         <Leftside />
-        <ScrollTimeline/>
+        <ScrollTimeline />
         <Rightside />
       </div>
-      <ContactSection/>
+      <Contact_us
+        heading="Take the First Step Toward Successful Change"
+        line1="Don’t let your change initiatives fall into the 70% that fail. Are you ready to turn change into  a competitive advantage?"
+        line2="Sign up for Our Change Management Programs Now"
+        button_Name="Get In Touch"
+      />
     </div>
   );
 }
